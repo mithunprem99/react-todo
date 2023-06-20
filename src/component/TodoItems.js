f7310070be43f367a54570e1f319c57ba8f05384
mithunprem99 <mithunprem1234@gmail.com>
2023-06-20 14:59:22 +0530

    updated todoitems.js

--- src/component/TodoItems.js
+++ src/component/TodoItems.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
 export const TodoItems = (props) => {
-  return (
-    <div>
-        <input type='checkbox' name='checkbox' onChange={() => props.completed(props.id)}/>
-        {props.item}
-    </div>
-  )
+    return (
+        <div className='todo-item'>
+            <input type='checkbox' name='checkbox' onChange={() => props.completed(props.id)} />
+           <p  className='ml-1'> {props.item}</p>
+        </div>
+    )
 }
