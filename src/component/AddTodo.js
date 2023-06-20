4d36594a53e729399f014c1d07ac3ab413deabdd
mithunprem99 <mithunprem1234@gmail.com>
2023-06-20 14:17:35 +0530

    updated App.js

--- src/component/AddTodo.js
+++ src/component/AddTodo.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import { useState } from 'react'
+export const AddTodo = (props) => {
+    const [item, setItem] = useState("")
 
-export const AddTodo = () => {
-  return (
-    <form method='POST'>
-        <input type='text' placeholder='Enter todos..' name='todo'/>
-        <button>ADD</button>
+    const handleChange = (ev) =>{
+        setItem(ev.target.value)
+    }
+
+    const handleSubmit = (e) =>{
+        e.preventDefault()
+        props.item(item)
+        console.log(item)
+    }
+    
+    return (
+    <form method='POST' onSubmit={handleSubmit}>
+        <input type='text' placeholder='Enter todos..' name='todo' value={item} onChange={handleChange}/>
+        <button type='submit'>ADD</button>
     </form>
   )
 }
