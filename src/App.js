d59c038dd5591554d2a3d855970f322b32a2763f
mithunprem99 <mithunprem1234@gmail.com>
2023-06-20 14:16:38 +0530

    updated App.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,28 @@ import './App.css';
 import { AddTodo } from './component/AddTodo';
 import { TodoItems } from './component/TodoItems';
 function App() {
+  const [todo,setTodo] = useState([])
+  const getItem = (item) =>{
+    // console.log(item)
+    setTodo((prevState)=>{
+      return [...prevState,item]
+    })
+  }
+  const getCompleted = (id) =>{
+    console.log(id);
+    setTodo((prevState)=>{
+      return prevState.filter((item, index)=>{
+        return index !== id;
+      })
+    })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-      <AddTodo />
-      <TodoItems />
-      </header>
+    <div>
+      
+      <AddTodo item = {getItem}/>
+      {todo && todo.map((todo, index) =>(
+      <TodoItems id={index} item = {todo} completed={getCompleted}/>
+      ))}
     </div>
   );
 }
